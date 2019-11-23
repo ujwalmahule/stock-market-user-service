d7b446505edb41c8e0448b905943621ad191f2cd
UjwalAnilMahule <ujwalmahule@cn.ibm.com>
2019-11-23 18:46:42 +0800

    initial

---
+++ src/main/java/com/ujwal/users/UserServiceApplication.java
@@ -0,0 +1,23 @@
+package com.ujwal.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@RestController
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+	
+	@RequestMapping(value = "/")
+	public String home() {
+		return "Eureka Client application";
+	}
+}
+	
---
+++ src/main/java/com/ujwal/users/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.ujwal.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/users")
+	public String getAllUsers() {
+		return "Hello";
+	}
+}
