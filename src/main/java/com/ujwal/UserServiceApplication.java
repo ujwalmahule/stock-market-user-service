02fe064885a95051d742a63b68af74d47bd3167e
UjwalAnilMahule <ujwalmahule@cn.ibm.com>
2019-11-27 22:22:39 +0800

    refactor code

--- src/main/java/com/ujwal/UserServiceApplication.java
+++ src/main/java/com/ujwal/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.ujwal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
